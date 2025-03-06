5e24ed3b06cfeef33f81cefa11085c76a5a9513c
Tely Lor <tlor1@ewu.edu>
2025-03-06 17:06:49 +0000

    updated

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ console.log("👍 Okay");
 
 // Age of Person, Place, or Thing
 // Unit Test
-const birthDate = "1973-09-29"; // YYYY-MM-DD format
+const birthDate = "2005-05-19"; // YYYY-MM-DD format
 // const birthDateOfSwoop = "September 29, 1973"; // Month DD, YYYY
 // const birthDateOfSwoop = "09/09/1973"; // MM/DD/YYYY
 const ageOfPerson = calculateAgeOf( birthDate );
@@ -23,3 +23,23 @@ const elemAgeOfPerson = document.querySelector('.ageOfPerson');
 // Set to HTML element
 elemAgeOfPerson.textContent = ageOfPerson;
 elemAgeOfPerson.setAttribute("datetime", ageOfPerson );
+
+//Graduation Date
+const eventDate = "2025-06-13";
+const gradDays = getDaysUntilEvent( eventDate );
+console.log( gradDays );
+
+const elemDaysUntilGrad = document.querySelector('.daysUntilEvent');
+elemDaysUntilGrad.textContent = gradDays;
+elemDaysUntilGrad.setAttribute("datetime", gradDays );
+
+
+const startDate = "2005-05-19";
+const streakOfMeditation = getDayStreak(startDate);
+console.log( streakOfMeditation );
+
+// Get HTML element
+const elemMeditationStreak = document.querySelector('.streakOfDays');
+// Set to HTML element
+elemMeditationStreak.textContent = streakOfMeditation;
+elemMeditationStreak.setAttribute("datetime", streakOfMeditation );
